8cc51cfda0c147104fd51362704568188a8292ba
rhijmans <r.hijmans@gmail.com>
2023-11-25 08:14:22 -0800

    export adjust_range

--- R/misc.R
+++ R/misc.R
@@ -1,28 +1,26 @@
 
 remove_small <- function(range_adj, intr, d) {
 
-  a <- terra::expanse(range_adj, unit="km")
-  intr <- terra::disagg(intr)
-  intr$area <- terra::expanse(intr, unit="km")
-  x = intr[intr$area> d,]
-  y = intr[intr$area<= d,]
-  y = terra::disagg(terra::aggregate(y, dissolve=TRUE))
-  terra::values(x) = NULL
-  terra::values(y) = NULL
-  if (nrow(y) > 0 & nrow(x) > 0) {
-    intr <- terra::combineGeoms(x, y, boundary=TRUE, distance=TRUE)
-    intr$area <- terra::expanse(intr, unit="km")
-  }
-  intr
+	a <- terra::expanse(range_adj, unit="km")
+	intr <- terra::disagg(intr)
+	intr$area <- terra::expanse(intr, unit="km")
+	x <- intr[intr$area> d,]
+	y <- intr[intr$area<= d,]
+	y <- terra::disagg(terra::aggregate(y, dissolve=TRUE))
+	terra::values(x) <- NULL
+	terra::values(y) <- NULL
+	if (nrow(y) > 0 & nrow(x) > 0) {
+		intr <- terra::combineGeoms(x, y, boundary=TRUE, distance=TRUE)
+		intr$area <- terra::expanse(intr, unit="km")
+	}
+	intr
 }
 
 
-adjust_range <- function(x, sp, th, CAmin=100000, CAmax=250000) { 
-	r <- x > th
+adjust_range <- function(x, sp, CAmin=50000, CAmax=250000) { 
 	ca_add <- terra::buffer(sp, CAmin) |> terra::aggregate()
 	ca_remove <- terra::buffer(sp, CAmax) |> terra::aggregate()
-	m <- terra::mask(r, ca_remove, updatevalue=FALSE)
-	r <- (m + r) > 1
-	terra::rasterize(ca_add, r, update=TRUE)
+	x <- terra::mask(x, ca_remove, updatevalue=NA)
+	terra::rasterize(ca_add, x, update=TRUE)
 }
 
