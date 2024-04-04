97db80d4c965cb047c93f05f97c054747e1e6070
rhijmans <r.hijmans@gmail.com>
2024-04-04 09:57:55 -0700

    envd

--- R/exsitu.R
+++ R/exsitu.R
@@ -62,7 +62,59 @@ small_ssize_penalty <- function(ssize, score, minssize=10) {
 }
 
 
-ex_cvs <- function(regions, sample, env=NULL, adjust=TRUE, minssize=10) {
+ex_cvs_old <- function(regions, sample, env=NULL, adjust=FALSE, minssize=10) {
+
+## TODO  RH
+# fix the adjust effect such that when you have many observations in one zones
+# they can only contribute to their neighbors. Do not increase branch length to avoid that
+# one region does not compensate for another
+
+	if (minssize <= 0) {
+		return(0)
+	}
+		
+	if (nrow(regions) == 1) {
+		# cannot make a tree 
+		s <- terra::extract(regions, sample)
+		if (length(na.omit(terra::extract(regions, s)[,2])) > 1) { 
+			return(small_ssize_penalty(length(sample), 1, minssize))
+		} else {
+			return(0)		
+		}
+	}
+		
+	xy <- terra::centroids(regions)
+	d <- terra::distance(xy, unit="km")
+	if (!is.null(env)) {
+		e <- terra::extract(env, regions, fun=mean, na.rm=TRUE)
+		ed1 <- dist(e[,2])
+		ed2 <- dist(e[,3])		
+	} 
+	
+	a <- cbind(g=as.vector(d), tmp=as.vector(ed1), prc=as.vector(ed2))
+	
+	
+	x <- stats::hclust(d)
+	x <- ape::as.phylo(x)
+	terra::values(regions) <- NULL
+	
+	# if nsamples for a region > threshold (10) one neighbor that is empty can get an observation
+	##
+	
+	s <- terra::extract(regions, sample)
+
+	actual_pd <- branch_length(x, s[,2], adjust=adjust)	
+	potential_pd <- branch_length(x, 1:nrow(regions), adjust=adjust)
+	score <- min(1, actual_pd/potential_pd)
+	
+	small_ssize_penalty(length(sample), score, minssize)
+	
+}
+
+
+
+
+ex_cvs <- function(regions, sample, env=NULL, envfun=NULL, adjust=FALSE, minssize=10) {
 
 ## TODO  RH
 # fix the adjust effect such that when you have many observations in one zones
@@ -70,25 +122,32 @@ ex_cvs <- function(regions, sample, env=NULL, adjust=TRUE, minssize=10) {
 # one region does not compensate for another
 
 	stopifnot(minssize > 0)
+
+	if (nrow(sample) <= 0) {
+		return(0)
+	}
 		
 	if (nrow(regions) == 1) {
 		# cannot make a tree 
-		s <- terra::extract(regions, sample)
-		if (length(na.omit(terra::extract(regions, s)[,2])) > 1) { 
-			return(1)
-		} else {
-			return(0)		
-		}
+		return(small_ssize_penalty(nrow(sample), 1, minssize))
 	}
 		
 	xy <- terra::centroids(regions)
+	d <- terra::distance(xy, unit="km")
 	if (!is.null(env)) {
-		# use the ClustGeo approach?
-		e <- terra::extract(env, regions, fun=mean, na.rm=TRUE)
-		d <- terra::distance(cbind(xy, e))
-	} else {
-		d <- terra::distance(xy, unit="km")
-	}
+		e <- terra::extract(env, regions, fun=mean, na.rm=TRUE, ID=FALSE)
+		ed1 <- dist(e[,1])
+		ed2 <- dist(e[,2])	
+		ed <- data.frame(ed1, ed2)
+		names(ed) <- names(env)
+		# express environmental distance expressed as geographic distance
+		envd <- envfun(ed)
+		envd <- structure(envd, class = 'dist', Size=attr(ed1, "Size"))
+		# env dist cannot be smaller than geo dist
+		envd[envd < d] <- d[envd < d]
+		# mean of geo and env dist
+		d <- (d + envd) / 2
+	} 
 
 	x <- stats::hclust(d)
 	x <- ape::as.phylo(x)
